c6d13dca5d948e7bf022a69ffe151516c589e15d
Benson Grotegut <gutblue@Bensons-MacBook-Air.local>
2025-02-07 18:12:09 -0700

    Initial commit

---
+++ GuitarHobbyWebApp/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function() {
+    $("#calculate").click(function() {
+        let hours = $("#hours").val();
+        let rate = $("#rate").val();
+
+        if (hours > 0) {
+            let total = hours * rate;
+            $("#total").text(`$${total.toFixed(2)}`);
+        } else {
+            alert("Please enter a valid number of hours.");
+        }
+    });
+});
